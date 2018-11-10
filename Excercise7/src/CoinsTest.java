ceb5d219f006602974fc8802d35fe8bc086eb85b
Aaron Yang <hua3467@yahoo.com>
2018-11-10 15:59:18 -0600

    Revert "Exercise8"
    This reverts commit 7de06ab0275d02e3410d7172d8f634397e30490e.

--- Excercise7/src/CoinsTest.java
+++
@@ -1,7 +0,0 @@
-public class CoinsTest {
-    public static void main( String [] args ){
-        Coins piggyBank = new Coins( 5, 4, 4, 8 );
-
-        System.out.println( piggyBank.toString());
-    }
-}
