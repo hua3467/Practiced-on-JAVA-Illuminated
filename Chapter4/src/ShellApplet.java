c8fbf3ef8b1dc309f8c27c742490471b0d901c8b
Aaron Yang <hua3467@yahoo.com>
2018-11-13 21:16:20 -0600

    Chapter 4 Examples_init

---
+++ Chapter4/src/ShellApplet.java
@@ -0,0 +1,14 @@
+
+/*  Example 4.1
+    An applet shell
+    Anderson, Franceschi
+ */
+
+import javax.swing.JApplet;
+import java.awt.Graphics;
+
+public class ShellApplet extends JApplet {
+   public void paint ( Graphics g ){
+       super.paint( g );
+   }
+}
